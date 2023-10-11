b801bc7114a2143e6c4f61bb19f4bd6971cc8d5c
rob <rob_abcba@yahoo.com>
2023-10-11 11:46:49 -0700

    Linting fixes

--- 08 user model code.js
+++ 08 user model code.js
@@ -1,72 +1,72 @@
-const mongoose = require('mongoose');
-const crypto = require('crypto');
+const mongoose = require("mongoose");
+const crypto = require("crypto");
 
 const userSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            trim: true,
-            required: true,
-            max: 32
-        },
-        email: {
-            type: String,
-            trim: true,
-            required: true,
-            unique: true,
-            lowercase: true
-        },
-        hashed_password: {
-            type: String,
-            required: true
-        },
-        salt: String,
-        role: {
-            type: String,
-            default: 'subscriber'
-        },
-        resetPasswordLink: {
-            data: String,
-            default: ''
-        }
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      max: 32,
     },
-    { timestamps: true }
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    hashed_password: {
+      type: String,
+      required: true,
+    },
+    salt: String,
+    role: {
+      type: String,
+      default: "subscriber", //MC: Seller, Buyer, Admin, Seller/Buyer
+    },
+    resetPasswordLink: {
+      data: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
 );
 
 userSchema
-    .virtual('password')
-    .set(function(password) {
-        // create a temporarity variable called _password
-        this._password = password;
-        // generate salt
-        this.salt = this.makeSalt();
-        // encryptPassword
-        this.hashed_password = this.encryptPassword(password);
-    })
-    .get(function() {
-        return this._password;
-    });
+  .virtual("password")
+  .set(function (password) {
+    // create a temporarity variable called _password
+    this._password = password;
+    // generate salt
+    this.salt = this.makeSalt();
+    // encryptPassword
+    this.hashed_password = this.encryptPassword(password);
+  })
+  .get(function () {
+    return this._password;
+  });
 
 userSchema.methods = {
-    authenticate: function(plainText) {
-        return this.encryptPassword(plainText) === this.hashed_password;
-    },
+  authenticate: function (plainText) {
+    return this.encryptPassword(plainText) === this.hashed_password;
+  },
 
-    encryptPassword: function(password) {
-        if (!password) return '';
-        try {
-            return crypto
-                .createHmac('sha1', this.salt)
-                .update(password)
-                .digest('hex');
-        } catch (err) {
-            return '';
-        }
-    },
-
-    makeSalt: function() {
-        return Math.round(new Date().valueOf() * Math.random()) + '';
+  encryptPassword: function (password) {
+    if (!password) return "";
+    try {
+      return crypto
+        .createHmac("sha1", this.salt)
+        .update(password)
+        .digest("hex");
+    } catch (err) {
+      return "";
     }
+  },
+
+  makeSalt: function () {
+    return Math.round(new Date().valueOf() * Math.random()) + "";
+  },
 };
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
--- 13 signup user/server/controllers/auth.js
+++ 13 signup user/server/controllers/auth.js
@@ -1,28 +1,29 @@
-const User = require('../models/user');
+const User = require("../models/user");
 
 exports.signup = (req, res) => {
-    // console.log('REQ BODY ON SIGNUP', req.body);
-    const { name, email, password } = req.body;
+  // console.log('REQ BODY ON SIGNUP', req.body);
+  const { name, email, password } = req.body; //MC: 13 signup user/server/controllers/auth.js is FIRST time we check whether requested email has already been taken.
+  //MC: Destructure name, email, and password from req.body
+  User.findOne({ email }).exec((err, user) => {
+    //MC: findOne({email:email}) can be simplified to findOne({email})
+    if (user) {
+      return res.status(400).json({
+        error: "Email is taken",
+      });
+    }
+  });
 
-    User.findOne({ email }).exec((err, user) => {
-        if (user) {
-            return res.status(400).json({
-                error: 'Email is taken'
-            });
-        }
+  let newUser = new User({ name, email, password });
+
+  newUser.save((err, success) => {
+    if (err) {
+      console.log("SIGNUP ERROR", err);
+      return res.status(400).json({
+        error: err,
+      });
+    }
+    res.json({
+      message: "Signup success! Please signin",
     });
-
-    let newUser = new User({ name, email, password });
-
-    newUser.save((err, success) => {
-        if (err) {
-            console.log('SIGNUP ERROR', err);
-            return res.status(400).json({
-                error: err
-            });
-        }
-        res.json({
-            message: 'Signup success! Please signin'
-        });
-    });
+  });
 };
