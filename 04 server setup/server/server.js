20c8ee6aadb9f9a6ace657b16efe2e95680ce981
rob <rob_abcba@yahoo.com>
2023-10-11 17:25:00 -0700

    Move res.json data message from 04_server.js to 07_controllers/auth.js

--- 04 server setup/server/server.js
+++ 04 server setup/server/server.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('/api/signup', (req, res) => {
-    res.json({
-        data: 'you hit signup endpoint'
-    });
+app.get("/api/signup", (req, res) => {
+  res.json({
+    data: "you hit signup endpoint", //MC: Will move this to server/controllers/auth.js in 07_moving_to_controllers
+  });
 });
 
 const port = process.env.port || 8000;
 app.listen(port, () => {
-    console.log(`API is running on port ${port}`);
+  console.log(`API is running on port ${port}`);
 });
--- 07 moving to controllers/server/controllers/auth.js
+++ 07 moving to controllers/server/controllers/auth.js
@@ -1,5 +1,5 @@
 exports.signup = (req, res) => {
-    res.json({
-        data: 'you hit signup endpoint yay from controllers'
-    });
+  res.json({
+    data: "you hit signup endpoint yay from controllers", //MC: Moved from server.js to here in servers/controllers/auth.js in 07_moving_to_controllers chapter
+  });
 };
